6b2cfd5833276255bb001ad4cf3a3abe70ea2c57
Shafa007 <80678467+Shafa007@users.noreply.github.com>
2021-07-13 19:14:01 +0600

    database read

---
+++ dbread.php
@@ -0,0 +1,14 @@
+<?php 
+required 'DBconnection.php';
+
+    function register($userName, $password){
+    	$conn = connect();
+    	$sql = $conn -> prepare("INSERT INTO USER (username, password) VALUES (?,?)");
+
+    	$sql -> bind_param("ss", $userName, $password);
+    	$response = $sql -> execute();
+    	$conn->close();
+    	return $response;
+    }
+
+?>
