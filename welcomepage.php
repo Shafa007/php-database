f90724c56ab3a2fd34b2ce7020cc87b4884ffb00
Shafa007 <80678467+Shafa007@users.noreply.github.com>
2021-07-15 23:25:47 +0600

    Modified Welcomepage

--- welcomepage.php
+++ welcomepage.php
@@ -1,6 +1,5 @@
 <?php 
 	session_start();
-	$userId = isset($_SESSION['uid']) ? $_SESSION['uid'] : ""; 
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,11 +9,19 @@
 </head>
 <body style="background-color: #CCD1D1 ">
 
-	<h1>Welcome, <?php echo $userId; ?></h1>
+	<h1>Welcome, <?php if(isset($_SESSION['username'])){ echo $_SESSION['username'];
+}
+else{
+	header("Location: logout.php");
+}
+?>
+	</h1>
+
 	<br><br>
 	<h2>
 	<p align="right";>
     <a href="logout.php">Logout</a></h2></p><br>
+    
 
     
 </body>
